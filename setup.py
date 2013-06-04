b5fff6a69bf8874309ddb786a782a869e14dc0d2
Ewen Cheslack-Postava <echeslack@gmail.com>
2013-06-04 14:49:39 -0700

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ pyhashxx = Extension('pyhashxx',
 
 setup(
     name = "pyhashxx",
-    version = "0.1",
+    version = "0.1.1",
     description = "Python wrapper for xxHash algorithm",
     author = "Ewen Cheslack-Postava",
     author_email = 'me@ewencp.org',
