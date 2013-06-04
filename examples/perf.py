152989a51571327e5cb65f1f908a5275bf45fdc8
Ewen Cheslack-Postava <echeslack@gmail.com>
2013-06-04 11:26:25 -0700

    Make perf example compare to smhasher if available.

--- examples/perf.py
+++ examples/perf.py
@@ -1,6 +1,13 @@
 from __future__ import print_function
-from pyhashxx import hashxx
 import random, time
+
+from pyhashxx import hashxx
+hashfns = [hashxx]
+try:
+    import smhasher
+    hashfns += [getattr(smhasher, k) for k in dir(smhasher) if k[0] == 'm']
+except:
+    pass
 
 print("Generating random data")
 chars = b'abcdefghijklmnopqrstuvwxyz'
@@ -11,15 +18,18 @@ long = b''.join([bytes(random.choice(chars)) for i in range(long_size)])
 extra_long_size = 64*1024*1024
 extra_long = long * 64
 
-def time_trial(size, data, number=1000000):
+def time_trial(hashfn, size, data, number=1000000):
     started = time.time()
     # Avoid loop overhead
-    [hashxx(data) for i in range(number)]
+    [hashfn(data) for i in range(number)]
     finished = time.time()
     duration = finished - started
-    print("%d bytes %d times, %f s, %f ms/hash, %f hashes/s, %f MB/s" % (size, number, duration, duration/number*1000, number/duration, number*size/(duration*1024*1024)))
+    print(" %d bytes %d times, %f s, %f ms/hash, %f hashes/s, %f MB/s" % (size, number, duration, duration/number*1000, number/duration, number*size/(duration*1024*1024)))
 
 if __name__ == "__main__":
-    time_trial(short_size, short, number=10000000)
-    time_trial(long_size, long, number=250000)
-    time_trial(extra_long_size, extra_long, number=10000)
+    for hashfn in hashfns:
+        print("%s:" % hashfn.__name__)
+        time_trial(hashfn, short_size, short, number=10000000)
+        time_trial(hashfn, long_size, long, number=250000)
+        time_trial(hashfn, extra_long_size, extra_long, number=10000)
+        print()
